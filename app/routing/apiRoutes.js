128b4bddb2c4bfa69f3b24c2c84f9939699ae3f6
Craig Dupuis <craigdupuis@Craig-Dupuis-MacBook-Pro.local>
2018-02-19 08:57:23 -0600

    adds comments and link to /api/friends array

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,13 @@
-var path = require('path');
+var path = require("path");
+
+//Links friends array
+var friends = require("../data/friends.js");
 
 module.exports = function(app) {
 
 app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
+
+
 };
