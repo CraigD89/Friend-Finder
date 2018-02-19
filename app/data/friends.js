1962ee7927e1bda5807bf833a3de14808a107679
Craig Dupuis <craigdupuis@Craig-Dupuis-MacBook-Pro.local>
2018-02-19 09:05:32 -0600

    adds values to friend arrays

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,16 +4,16 @@ var friendArr = [
 		name: "Joyce",
 		imageURL: "https://i.pinimg.com/736x/5a/8e/20/5a8e20a3d617e4cd425634029377c788--cat-evolution-future-cat.jpg",
 		scores: [
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			""
+			"5",
+			"4",
+			"2",
+			"3",
+			"2",
+			"1",
+			"4",
+			"5",
+			"3",
+			"5"
 		]
     },
     
@@ -21,16 +21,16 @@ var friendArr = [
 		name: "Trixie",
 		imageURL: "http://www.catster.com/wp-content/uploads/2017/09/A-tabby-cat-with-an-ID-collar-on.jpg",
 		scores: [
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			""
+			"1",
+			"2",
+			"4",
+			"3",
+			"4",
+			"5",
+			"2",
+			"1",
+			"3",
+			"1"
 		]
     },
     
@@ -55,16 +55,16 @@ var friendArr = [
 		name: "Michael",
 		imageURL: "http://www.insidedogsworld.com/wp-content/uploads/2017/07/German-Shepherd-Standard-Coat-GSC-1000x575-1-1-1-1.jpg",
 		scores: [
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			""
+			"1",
+			"2",
+			"3",
+			"4",
+			"5",
+			"5",
+			"4",
+			"3",
+			"2",
+			"1"
 		]
     },
 
@@ -72,16 +72,16 @@ var friendArr = [
 		name: "Scott",
 		imageURL: "https://thehappypuppysite.com/wp-content/uploads/2017/10/goldie.jpg",
 		scores: [
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-			""
+			"5",
+			"4",
+			"3",
+			"2",
+			"1",
+			"1",
+			"2",
+			"3",
+			"4",
+			"5"
 		]
     },
 
