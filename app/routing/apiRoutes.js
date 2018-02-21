4f52726e2d05caa20223ce3cbe9cde6205d47964
Craig Dupuis <craigdupuis@Craig-Dupuis-MacBook-Pro.local>
2018-02-21 16:09:14 -0600

    adds functions to apiRoutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,6 +2,7 @@ var path = require("path");
 
 //Links friend arrays
 var friends = require("../data/friends.js");
+    // console.log("Friends are ", friends);
 
 module.exports = function(app) {
   
@@ -10,5 +11,34 @@ module.exports = function(app) {
     res.json(friends);
   });
 
+  //Add user input to friend arrays
+  app.post("/api/friends", function (req, res){
+    //Takes in user's input as object
+    var input = req.body;
+    var inputScore = input.scores;
+
+    //Calculate match
+    var matchName = "";
+    var matchImage = "";
+    var totalDifference = 0;
+
+    //Go through friend array
+    for (var i = 0; i < friends.length; i++) {
+
+      //Compare to friend arrays
+      var difference = 0
+      for (var k = 0; k < inputScore.length; k++) {
+				difference += Math.abs(friends[i].scores[k] - inputScore[k]);
+      }
+      
+      //Determine match based on lowest difference
+      if (difference < totalDifference){
+        totalDifference = difference;
+				matchName = friends[i].name;
+				matchImage = friends[i].imageURL;
+      }
+    };
+  
+  });
 
 };
